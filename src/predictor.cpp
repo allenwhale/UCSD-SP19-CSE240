c703d3eb9a82ac26c5840eb40bf96767c0d8edbf
allenwhale <allencat850502@gmail.com>
2019-04-26 23:37:30 +0800

    change to c++

--- src/predictor.cpp
+++ src/predictor.cpp
@@ -5,23 +5,22 @@
 //  Implement the various branch predictors below as      //
 //  described in the README                               //
 //========================================================//
-#include <stdio.h>
 #include "predictor.h"
+#include <stdio.h>
 
 //
 // TODO:Student Information
 //
 const char *studentName = "NAME";
-const char *studentID   = "PID";
-const char *email       = "EMAIL";
+const char *studentID = "PID";
+const char *email = "EMAIL";
 
 //------------------------------------//
 //      Predictor Configuration       //
 //------------------------------------//
 
 // Handy Global for use in output routines
-const char *bpName[4] = { "Static", "Gshare",
-                          "Tournament", "Custom" };
+const char *bpName[4] = {"Static", "Gshare", "Tournament", "Custom"};
 
 int ghistoryBits; // Number of bits used for Global History
 int lhistoryBits; // Number of bits used for Local History
@@ -34,9 +33,8 @@ int verbose;
 //------------------------------------//
 
 //
-//TODO: Add your own Branch Predictor data structures here
+// TODO: Add your own Branch Predictor data structures here
 //
-
 
 //------------------------------------//
 //        Predictor Functions         //
@@ -44,48 +42,42 @@ int verbose;
 
 // Initialize the predictor
 //
-void
-init_predictor()
-{
-  //
-  //TODO: Initialize Branch Predictor Data Structures
-  //
+void init_predictor() {
+    //
+    // TODO: Initialize Branch Predictor Data Structures
+    //
 }
 
 // Make a prediction for conditional branch instruction at PC 'pc'
 // Returning TAKEN indicates a prediction of taken; returning NOTTAKEN
 // indicates a prediction of not taken
 //
-uint8_t
-make_prediction(uint32_t pc)
-{
-  //
-  //TODO: Implement prediction scheme
-  //
+uint8_t make_prediction(uint32_t pc) {
+    //
+    // TODO: Implement prediction scheme
+    //
 
-  // Make a prediction based on the bpType
-  switch (bpType) {
+    // Make a prediction based on the bpType
+    switch (bpType) {
     case STATIC:
-      return TAKEN;
+        return TAKEN;
     case GSHARE:
     case TOURNAMENT:
     case CUSTOM:
     default:
-      break;
-  }
+        break;
+    }
 
-  // If there is not a compatable bpType then return NOTTAKEN
-  return NOTTAKEN;
+    // If there is not a compatable bpType then return NOTTAKEN
+    return NOTTAKEN;
 }
 
 // Train the predictor the last executed branch at PC 'pc' and with
 // outcome 'outcome' (true indicates that the branch was taken, false
 // indicates that the branch was not taken)
 //
-void
-train_predictor(uint32_t pc, uint8_t outcome)
-{
-  //
-  //TODO: Implement Predictor training
-  //
+void train_predictor(uint32_t pc, uint8_t outcome) {
+    //
+    // TODO: Implement Predictor training
+    //
 }
